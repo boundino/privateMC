b693e25d8a31cd16d47c89474191cab7c6d838ab
boundino <boundino@gmail.com>
2025-08-02 02:21:40 +0200

    up reco

--- MCprod/run_mc_reco_cmssw150X.sh
+++ MCprod/run_mc_reco_cmssw150X.sh
@@ -1,11 +1,10 @@
 set -x
-cmsDriver.py reco --mc --eventcontent AODSIM --datatier AODSIM \
-             --conditions 150X_mcRun3_2025_forOO_realistic_v7 \
-             --geometry DB:Extended \
-             --step RAW2DIGI,L1Reco,RECO,RECOSIM --era Run3_2025_OXY \
+cmsDriver.py reco --mc --era Run3_2025_OXY \
+             --step RAW2DIGI,L1Reco,RECO \
+             --datatier AODSIM --eventcontent AODSIM \
+             --conditions 150X_mcRun3_2025_forOO_realistic_v8 \
+             --customise_commands "process.AODSIMoutput.outputCommands.extend(['keep *_mix_MergedTrackTruth_*', 'keep *Link*_simSiPixelDigis__*', 'keep *Link*_simSiStripDigis__*', 'keep *_generalTracks__*', 'keep *_hiConformalPixelTracks__*', 'keep *_siPixelClusters__*', 'keep *_siStripClusters__*'])" \
              --nThreads 4 \
-             --customise_commands "process.AODSIMoutput.outputCommands.extend(['keep *_mix_MergedTrackTruth_*', 'keep *Link*_simSiPixelDigis__*', 'keep *Link*_simSiStripDigis__*', 'keep *_generalTracks__*', 'keep *_hiConformalPixelTracks__*', 'keep *_siPixelClusters__*', 'keep *_siStripClusters__*', 'keep *_towerMaker_*_*'])" \
              --no_exec -n -1 || exit $? ;
-
 set +x
 
