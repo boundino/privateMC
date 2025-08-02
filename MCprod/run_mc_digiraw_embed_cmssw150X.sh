247c4643288aa8d0337412122b9a5c764af8c1e1
boundino <boundino@gmail.com>
2025-08-02 02:04:01 +0200

    up digiraw

--- MCprod/run_mc_digiraw_embed_cmssw150X.sh
+++ MCprod/run_mc_digiraw_embed_cmssw150X.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 set -x
-cmsDriver.py digiraw --mc --pileup HiMixNoPU --era Run3_2025_OXY \
+cmsDriver.py digirawembed --mc --pileup HiMix --era Run3_2025_OXY \
              --step DIGI:pdigi_hi_nogen,L1,DIGI2RAW,HLT:PIon --geometry DB:Extended \
              --conditions 150X_mcRun3_2025_forOO_realistic_v8 \
              --customise_commands "process.RAWSIMoutput.outputCommands.extend(['keep *_mix_MergedTrackTruth_*', 'keep *Link*_simSiPixelDigis__*', 'keep *Link*_simSiStripDigis__*'])" \
              --datatier GEN-SIM-DIGI-RAW --eventcontent RAWSIM \
+             --pileup_input "dbs:/MinBias_Hijing_NeNe_5362GeV/wangj-GENSIM_250731_15011_Realistic_v8-022e8f21bdf6dc68bae0a2bb0c289cbd/USER instance=prod/phys03" \
              --nThreads 4 \
              --no_exec -n -1 || exit $? ;
 set +x
